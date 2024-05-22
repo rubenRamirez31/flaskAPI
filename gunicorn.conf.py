4a32ad166ded378512050955fe57ecc8a3ea1875
rubenRamirez31 <106482266+rubenRamirez31@users.noreply.github.com>
2024-05-22 00:38:50 -0600

    cambiando workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,3 @@
 
-workers = 1  # Número de workers para manejar las solicitudes
+workers = 4  # Número de workers para manejar las solicitudes
 bind = '0.0.0.0:8080'  # Especifica el host y puerto de escucha
